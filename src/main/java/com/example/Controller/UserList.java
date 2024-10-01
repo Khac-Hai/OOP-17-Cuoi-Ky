eb85ca91976d1b5c79184a99014c334c0610fca8
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-10-01 07:19:56 +0700

    Update UserList.java

--- src/main/java/com/example/Controller/UserList.java
+++ src/main/java/com/example/Controller/UserList.java
@@ -1,9 +1,18 @@
 import Modeles/User
+import java.util;
 
 public class UserList{
 
-  User u = new User(")
+    User u1 = new User(0101, "user1", "password1");
+    User u2 = new User(0102, "user2", "password2");
+    User u3 = new User(0103, "user3", "password3");
+    User u4 = new User(0104, "user4", "password4");
 
+   ArrayList<User> listUser = new ArrayList<User>;
+   ListUser.add(u1);
+   ListUser.add(u2);
+   ListUser.add(u3);
+   ListUser.add(u4);
   
 }  
   
