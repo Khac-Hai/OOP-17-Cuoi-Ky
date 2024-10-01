201a88f4d355be4153238153139e2456642c6a5e
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-10-01 07:21:49 +0700

    Update Login.java

--- src/main/java/com/example/Controller/Login.java
+++ src/main/java/com/example/Controller/Login.java
@@ -1,1 +1,8 @@
+/*Todo
++Kiem tra username va password trong login.fxml
++ Viet method kiem tra username va password co trong ListUser
++ Co, thi cho login
++ Khong, dang ki chuyen qua register.fxml
 
+
+*/
